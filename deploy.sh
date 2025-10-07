ac40c417a928249b8b416b5da8b9825d2fecfc63
sokphaladam <sokphaladam@gmail.com>
2025-10-08 01:27:03 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ cd /var/www/cafe-shop
 git fetch origin deploy-local
 if [ $(git rev-parse HEAD) != $(git rev-parse origin/deploy-local) ]; then
   echo "🔄 Changes detected, updating..."
+  git restore .
   git pull origin deploy-local
-  . ~/.nvm/nvm.sh && pnpm install --omit=dev
+  . ~/.nvm/nvm.sh && pnpm install
   . ~/.nvm/nvm.sh && pnpm run build
   . ~/.nvm/nvm.sh && pm2 restart next-app
   echo "✅ Updated successfully!!"
